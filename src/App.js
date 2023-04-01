44838ebc79d77176d2be45a9dbb816821267d021
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-04-01 14:14:27 +0800

    Create main container

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
+import { MainContainer } from './components/MainContainer';
 
 function App() {
   return (
     <div className="App">
       <div className="bg">
-        <div className="main-container">
-          <div className="timer">25:00</div>
-          <div className="start">Start</div>
-          <div className="stage">
-            <div>Pomodoro</div>
-            <div>Break</div>
-          </div>
-        </div>
+        <MainContainer/>
       </div>
     </div>
   );
---
+++ src/components/MainContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const MainContainer = () => {
+  return (
+    <div className="main-container">
+      <div className="timer">25:00</div>
+      <div className="start button">START</div>
+      <div className="stage">
+        <div className="button">WORK</div>
+        <div className="button">BREAK</div>
+      </div>
+    </div>
+  );
+};
