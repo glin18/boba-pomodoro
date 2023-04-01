993c93fac5b32b07c49a56140f050e9c851b2fb8
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-04-01 13:54:11 +0800

    Create basic main content structure

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ function App() {
   return (
     <div className="App">
       <div className="bg">
-
+        <div className="main-container">
+          <div className="timer">25:00</div>
+          <div className="start">Start</div>
+          <div className="stage">
+            <div>Pomodoro</div>
+            <div>Break</div>
+          </div>
+        </div>
       </div>
     </div>
   );
