e9135fdd52419df7f741c900fd6ba93ea170d492
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-04-06 14:29:42 +0800

    Create settings container component

---
+++ src/components/SettingsContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SettingsContainer = () => {
+  return (
+    <div className="settings-container">
+        <div className="settings-title">Boba Settings</div>
+    </div>
+  )
+}
