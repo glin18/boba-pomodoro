5315c3ca00840f39f7c7a6394da352ec04059f73
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-04-06 14:30:02 +0800

    Add functionality to settings button and add styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { useState } from "react";
 import { MainContainer } from './components/MainContainer';
+import { SettingsContainer } from "./components/SettingsContainer";
+import SettingsIcon from "./images/setting.png";
 
 function App() {
+  const [ showSettings, setShowSettings ] = useState(false);
+
   return (
     <div className="App">
       <div className="bg">
-        <MainContainer/>
+        <img className="settings-icon" src={SettingsIcon} onClick={()=>(setShowSettings(prevSettings => !prevSettings))}></img>
+        { showSettings ? <SettingsContainer/> : <MainContainer/>}
       </div>
     </div>
   );
